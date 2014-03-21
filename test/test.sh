9404e2108788a2a233aeb00c703eddf0d16a6d7d
Keita Obo <ktaobo@gmail.com>
2014-03-21 13:55:25 +0900

    Add tests for failure cases

--- test/test.sh
+++ test/test.sh
@@ -12,6 +12,23 @@ function error_exit() {
 function run() {
   echo -n "Testing $1 ... "
   result=$(./$1)
+  code=$?
+  if [ $code -ne 0 ]; then
+    error_exit "exit status is not 0 [$code]"
+  fi
+  if [ "$result" != "$2" ]; then
+    error_exit "$2 expected, but got '$result'"
+  fi
+  echo ok
+}
+
+function fail() {
+  echo -n "Testing $1 ... "
+  result=$(./$1)
+  code=$?
+  if [ $code -eq 0 ]; then
+    error_exit "Failure expected, but succeeded!"
+  fi
   if [ "$result" != "$2" ]; then
     error_exit "$2 expected, but got '$result'"
   fi
@@ -29,6 +46,10 @@ run class 'Foo::ctor(123)
 99980001
 Foo::dtor()'
 run module 'modfunc called: 1234'
+
+# Failure cases
+fail wrong_type "TypeError: can't convert String into Fixnum, argument 1(1111)"
+fail wrong_arg_num "ArgumentError: 'square': wrong number of arguments (2 for 1)"
 
 ################################################################
 # All tests succeeded.
---
+++ test/wrong_type.cc
@@ -0,0 +1,26 @@
+#include <mruby.h>
+#include <mruby/compile.h>
+#include "mrubybind.h"
+
+int square(int x) {
+  return x * x;
+}
+
+int main() {
+  mrb_state* mrb = mrb_open();
+
+  {
+    mrubybind::MrubyBind b(mrb);
+    b.bind("square", square);
+  }
+
+  int result_code = EXIT_SUCCESS;
+  mrb_load_string(mrb, "puts square(\"1111\")");
+  if (mrb->exc) {
+    mrb_p(mrb, mrb_obj_value(mrb->exc));
+    result_code = EXIT_FAILURE;
+  }
+
+  mrb_close(mrb);
+  return result_code;
+}
